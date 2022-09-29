823e0c05a819922c14847cc4f35d327a3c5251b9
geetNaina <gp@esokia-webagency.com>
2022-09-29 06:17:18 +0400

    position

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+
+// $(document).ready(function () {
+//     window.addEventListener('scroll', function () {
+//         var nav = document.querySelector('nav');
+//       nav.classList.toggle('sticky', window.scrollY > 0);
+//     })
+
+// });
+
+$(document).ready(function(){
+  $(".icon-bar").click(function() {
+    $("body").toggleClass("showing");
+  });
+});
+
+//     var nav = document.querySelector('nav');
+
+//     window.addEventListener('scroll', function () {
+//       if (window.pageYOffset > 100) {
+//         nav.classList.add('sticky');
+//       } else {
+//         nav.classList.remove('sticky');
+//       }
+//     });
+
+// });
+
